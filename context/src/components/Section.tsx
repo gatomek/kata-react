4641d632250614f213f4b8b77b802c322dffd11b
Gatomek <tomasz.galinski@gmail.com>
2025-09-07 00:38:55 +0200

    context - review

--- context/src/components/Section.tsx
+++ context/src/components/Section.tsx
@@ -1,7 +1,7 @@
 import {LevelContext} from "./LevelContext.tsx";
+import type {ListPropsWithLevel} from "./model/ListPropsWithLevel.ts";
 
-
-export default function Section({level, children}) {
+export default function Section({level, children}: Readonly<ListPropsWithLevel>) {
     return (
         <section>
             <LevelContext value={level}>
